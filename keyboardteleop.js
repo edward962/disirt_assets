b8075c9e24c2aeb751fa645c15b4ee3ef50798fd
ryzheuse <e.ryzheuski@accrea.com>
2020-12-03 11:37:04 +0100

    Added scripts

---
+++ keyboardteleop.js
@@ -0,0 +1,149 @@
+/**
+ * @author Russell Toris - rctoris@wpi.edu
+ */
+
+var KEYBOARDTELEOP = KEYBOARDTELEOP || {
+  REVISION : '0.4.0-SNAPSHOT'
+};
+
+/**
+ * @author Russell Toris - rctoris@wpi.edu
+ */
+
+/**
+ * Manages connection to the server and all interactions with ROS.
+ *
+ * Emits the following events:
+ *   * 'change' - emitted with a change in speed occurs
+ *
+ * @constructor
+ * @param options - possible keys include:
+ *   * ros - the ROSLIB.Ros connection handle
+ *   * topic (optional) - the Twist topic to publish to, like '/cmd_vel'
+ *   * throttle (optional) - a constant throttle for the speed
+ */
+KEYBOARDTELEOP.Teleop = function(options) {
+  var that = this;
+  options = options || {};
+  var ros = options.ros;
+  var topic = options.topic || '/cmd_vel';
+  // permanent throttle
+  var throttle = options.throttle || 1.0;
+
+  // used to externally throttle the speed (e.g., from a slider)
+  this.scale = 1.0;
+
+  // linear x and y movement and angular z movement
+  var x = 0;
+  var y = 0;
+  var z = 0;
+
+  var cmdVel = new ROSLIB.Topic({
+    ros : ros,
+    name : topic,
+    messageType : 'geometry_msgs/Twist'
+  });
+
+  // sets up a key listener on the page used for keyboard teleoperation
+  var handleKey = function(keyCode, keyDown) {
+    // used to check for changes in speed
+    var oldX = x;
+    var oldY = y;
+    var oldZ = z;
+    
+    var pub = true;
+
+    var speed = 0;
+    // throttle the speed by the slider and throttle constant
+    if (keyDown === true) {
+      speed = throttle * that.scale;
+    }
+    // check which key was pressed
+    switch (keyCode) {
+      case "KeyA":
+        // turn left
+        z = 1 * speed;
+        break;
+      case "KeyW":
+        // up
+        x = 0.5 * speed;
+        break;
+      case "KeyD":
+        // turn right
+        z = -1 * speed;
+        break;
+      case "KeyS":
+        // down
+        x = -0.5 * speed;
+        break;
+      case "KeyA":
+        // turn left
+        z = 1 * speed;
+        break;
+      case "KeyW":
+        // up
+        x = 0.5 * speed;
+        break;
+      case "KeyD":
+        // turn right
+        z = -1 * speed;
+        break;
+      case "KeyS":
+        // down
+        x = -0.5 * speed;
+        break;
+        case "ArrowLeft":
+        // turn left
+        z = 1 * speed;
+        break;
+      case "ArrowUp":
+        // up
+        x = 0.5 * speed;
+        break;
+      case "ArrowRight":
+        // turn right
+        z = -1 * speed;
+        break;
+      case "ArrowDown":
+        // down
+        x = -0.5 * speed;
+        break; 
+      default:
+        pub = false;
+    }
+
+    // publish the command
+    if (pub === true) {
+    //  console.log("Publishing to topic");
+      var twist = new ROSLIB.Message({
+        angular : {
+          x : 0,
+          y : 0,
+          z : z
+        },
+        linear : {
+          x : x,
+          y : y,
+          z : z
+        }
+      });
+      cmdVel.publish(twist);
+
+      // check for changes
+      if (oldX !== x || oldY !== y || oldZ !== z) {
+        that.emit('change', twist);
+      }
+    }
+  };
+
+  // handle the key
+  var body = document.getElementsByTagName('body')[0];
+  body.addEventListener('keydown', function(e) {
+   // console.log("Key code:",e.code);
+    handleKey(e.code, true);
+  }, false);
+  body.addEventListener('keyup', function(e) {
+    handleKey(e.code, false);
+  }, false);
+};
+KEYBOARDTELEOP.Teleop.prototype.__proto__ = EventEmitter2.prototype;
